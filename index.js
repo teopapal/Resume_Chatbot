844c23b7a0a039f1c310277bddece9562d4f79ba
teopapal <92680560+teopapal@users.noreply.github.com>
2025-11-24 03:49:36 +0200

    Changed backend IP

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const responses = require("./responses");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 25577;
 
 app.use(cors());
 app.use(express.json());
@@ -25,3 +25,5 @@ app.post("/chat", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+console.log("Ready!");
